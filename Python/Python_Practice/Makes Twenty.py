c24593395974bb1b895d7d95f1d5c08e86a647a8
Connor Welch <kudude@att.net>
2019-03-03 16:00:44 -0600

    Delete Makes Twenty.py

--- Python/Python_Practice/Makes Twenty.py
+++
@@ -1,8 +0,0 @@
-def makes_twenty(a, b):
-	return a == 20 or b == 20 or a + b == 20
-
-
-x = int(input("Input a number: "))
-y = int(input("Input another number: "))
-#print(f"{x}{y}")
-print(makes_twenty(x, y))
